9cea3a36bfeb160fc3df23663593c2a19a6a6625
Carolin <dev@carolin.co.uk>
2020-05-29 15:58:21 +0100

    one more update

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,25 +1,27 @@
 exports.handler = function(event, context, callback) {
-
     const secretContent = `
-    <h3> Welcome to the secret area</h3>
-    <p>Here we can tell you that the sky is <strong>blue</strong> and the twoplus two is four</p>
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `
+    
     let body
-
-    if(event.body) {
-        body = JSON.parse(event.body)
+  
+    if (event.body) {
+      body = JSON.parse(event.body)
     } else {
-        body = {}
+      body = {}
     }
-
-    if (body.passwort == "javascript") {
-        callback(null, {
-            statusCode: 200,
-            body: secretContent
-        })
+  
+    if (body.password == "javascript") {
+      callback(null, {
+        statusCode: 200,
+        body: secretContent
+      })
     } else {
-        callback(null, {
-            statusCode: 401            
-        })
+      callback(null, {
+        statusCode: 401
+      })
     }
-}
+  
+    
+  }
