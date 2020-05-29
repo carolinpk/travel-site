814aa2976c559be8dd561dfa40f7ea2b2d85d511
Carolin <dev@carolin.co.uk>
2020-05-29 15:16:43 +0100

    corrected callback spelling

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
-    calllback(null, {
+    callback(null, {
         statusCode: 200,
         body: "Welcome to the super secret area"
     })
