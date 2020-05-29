0f457196e399838287c1bf335a4ea9e45baf708e
Carolin <dev@carolin.co.uk>
2020-05-29 11:44:57 +0100

    completed lazyloading images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,10 @@
 import '../styles/styles.css';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-//import Modal from './modules/Modal';
 
 
-//new Modal();
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
